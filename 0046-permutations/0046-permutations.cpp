e3fa503fe57ffdd2353e946e0dab8672be73407e
mahendra <143242485+mahendra-chaudhary@users.noreply.github.com>
2025-07-27 17:23:45 +0530

    Time: 11 ms (6.81%), Space: 14.2 MB (5.11%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,15 +2,35 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
 
-        vector<vector<int>> result;
+        int n = nums.size();
 
-        sort(nums.begin(), nums.end()); // start with smallest lexicographical
-        do {
-            result.push_back(nums);
-        } while (next_permutation(nums.begin(), nums.end())); // generate next permutation
+        vector<vector<int>> result ;
+        if(n == 1){
+            vector<int> single;
+            single.push_back(nums[0]);
+            result.push_back(single);
+            return result;
+        }
+        for(int i = 0;i<n;i++){
+            int m = nums[i];
+            vector<int> remain;
+            for(int j = 0;j<n;j++){
+                if(j != i ){
+                    remain.push_back(nums[j]);
 
+                }
+            }
+            vector<vector<int>> perms = permute(remain);
+            for(vector<int>p:perms){
+                p.insert(p.begin(),m);
+                result.push_back(p);
+            }
+        }
+
+
+     
+            
         return result;
-    }
         
-    
+    }
 };
