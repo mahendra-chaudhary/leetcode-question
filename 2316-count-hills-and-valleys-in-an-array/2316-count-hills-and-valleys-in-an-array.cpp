fca5618a2be484df8f844c985c89535d4e7e3e77
mahendra <143242485+mahendra-chaudhary@users.noreply.github.com>
2025-07-27 10:27:55 +0530

    Time: 0 ms (100%), Space: 12 MB (96.23%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.cpp
@@ -6,11 +6,11 @@ public:
         int count = 0;
         int left = 0;
 
-        for(int i = 1 ;i<n-1;++i){
+        for(int i = 1 ;i<n-1;i++){
             if(nums[i] != nums[i+1]){
-            if((nums[i] > nums[left] && nums[i] > nums[i+1] ) ||
-               (nums[i] < nums[left] && nums[i] < nums[i+1])) {
-            ++count;
+            if((nums[i] > nums[left] && nums[i] > nums[i+1] ) ||(nums[i]<nums[left] && nums[i]<nums[i+1]))
+           {
+            count++;
            }
            left = i;
            }
