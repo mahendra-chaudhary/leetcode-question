86f6f342bb6c6c81c34072fc635afd7a0bd55c68
mahendra <143242485+mahendra-chaudhary@users.noreply.github.com>
2025-09-07 10:23:46 +0530

    Time: 3 ms (16.43%), Space: 8.4 MB (83.73%) - LeetHub

---
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    // int gcd(long long a, long long b) {
+    //     return b == 0 ? a : gcd(b, a % b);
+    // }
+    
+    int gcdOfOddEvenSums(int n) {
+        // long long sumOdd = 1LL * n * n;         // sum of first n odd numbers
+        // long long sumEven = 1LL * n * (n + 1);  // sum of first n even numbers
+        // return gcd(sumOdd, sumEven);
+        return n;
+    }
+};
