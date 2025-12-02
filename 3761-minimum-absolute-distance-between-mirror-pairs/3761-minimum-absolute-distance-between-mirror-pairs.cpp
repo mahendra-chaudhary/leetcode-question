a8f120b3db3271a445c3dc0b20af671868639fbe
mahendra <143242485+mahendra-chaudhary@users.noreply.github.com>
2025-12-02 12:25:08 +0530

    Time: 112 ms (70.27%), Space: 131.9 MB (75.56%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int reverseNumLL(long long x ){
-        long long rev = 0;
-        x = llabs(x);
-        while(x>0){
+    
+         int reverseNum(int x) {
+        int rev = 0;
+        while (x > 0) {
             int d = x % 10;
             rev = rev * 10 + d;
             x /= 10;
-
         }
         return rev;
     }
 
     int minMirrorPairDistance(vector<int>& nums) {
 
-        unordered_map<long long, int> lastIndex; // map value -> latest index j where reverse(nums[j]) == key
+            unordered_map<int, int> lastIndex; // value -> latest index i where reverse(nums[i]) == value
         int ans = INT_MAX;
 
-        for (int i = 0; i < (int)nums.size(); ++i) {
-            long long val = nums[i];
+        for (int i = 0; i < (int)nums.size(); i++) {
+            int val = nums[i];
+
+            // Step 1: treat i as right side j
             if (lastIndex.count(val)) {
                 ans = min(ans, i - lastIndex[val]);
             }
-            long long rev = reverseNumLL(val);
-            lastIndex[rev] = i;
+
+            // Step 2: now treat i as left side, prepare for future
+            int rev = reverseNum(val);
+            lastIndex[rev] = i;  // keep closest i from left for this 'rev'
         }
+
         return ans == INT_MAX ? -1 : ans;
     }
 };
