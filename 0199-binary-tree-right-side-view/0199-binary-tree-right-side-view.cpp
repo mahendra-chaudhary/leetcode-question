abf55d1040b79958c80ac71d31782aff1ac0243d
mahendra <143242485+mahendra-chaudhary@users.noreply.github.com>
2025-12-14 10:50:58 +0530

    Time: 0 ms (100%), Space: 15 MB (40.94%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,28 +11,60 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root,int level,vector<int>& ans){
-        if(!root) return ;
+    // useing dfs method can use 
+    // void dfs(TreeNode* root,int level,vector<int>& ans){
+    //     if(!root) return ;
 
-        if(level == ans.size()){
-            ans.push_back(root->val);
-
-        }
-        else{
-            ans[level] = root->val;
-
-
-        }
-
-        dfs(root->left,level+1,ans);
-        dfs(root->right,level+1,ans);
-    }
+    //     if(level == ans.size()){
+    //         ans.push_back(root->val);
+    //     }
+    //     else{
+    //         ans[level] = root->val;
+    //     }
+    //     dfs(root->left,level+1,ans);
+    //     dfs(root->right,level+1,ans);
+    // }
     vector<int> rightSideView(TreeNode* root) {
 
-        vector<int> ans;
-        dfs(root,0,ans);
+        // vector<int> ans;
+        // dfs(root,0,ans);
+
+        // return ans;
+
+
+        // using bfs process 
+
+        vector<int>ans;
+        if(!root) return ans;
+
+        queue<TreeNode*> q ;
+        q.push(root);
+
+        // TreeNode* node = root;
+
+        while(!q.empty()){
+
+            int size = q.size();
+
+            for(int i = 0;i<size;i++){
+
+                TreeNode* node = q.front();
+                q.pop();
+
+
+            if(i == size -1){
+                ans.push_back(node->val);
+
+            }
+
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+            }
+
+        }
 
         return ans;
+
 
 
         
