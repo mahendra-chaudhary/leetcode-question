b87717c35d8d9791f1da5afdaa1672577daeeb3b
mahendra <143242485+mahendra-chaudhary@users.noreply.github.com>
2026-06-23 11:32:15 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,21 +4,37 @@ public:
         int n = s.size();
         int m = t.size();
 
-        // if(n != m ) {
-        //  return false;
-        // }
+        if(n != m ) {
+         return false;
+        }
+
+        vector<int> freq(26,0);
+
+        for(char ch : s){
+            freq[ch - 'a']++;
+        } 
+        for(char ch : t){
+            freq[ch - 'a']--;
+        }
+
+        for(int count : freq){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
 
         // return true;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
 
 
-        if(s==t) {
-            return true;
-        }
+        // if(s==t) {
+        //     return true;
+        // }
 
-        return false;
+        // return false;
 
         
     }
